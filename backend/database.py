5a63ba3601bece9bbf72b08ddfd58142d603f08e
suyash-sketch <khandagalesuyash05@gmail.com>
2026-02-19 13:41:43 +0530

    added suyash's db password

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace 'your_actual_password_here' with the Postgres password you made during installation!
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/edumate"
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/edumate"
+SQLALCHEMY_DATABASE_URL = "postgresql://edumate_user:edumate_pass@localhost:5432/edumate"
 
 # Create the engine that talks to Postgres
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
